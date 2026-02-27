3b835483d9975ed883b6fa2623cbca6733862999
Sean Pettas <sedp@itu.dk>
2026-02-27 15:17:42 +0100

    first commit

---
+++ src/main/java/com/github/peachy006/storeboughtfinalreact/StoreBoughtFinalReactApplication.java
@@ -0,0 +1,13 @@
+package com.github.peachy006.storeboughtfinalreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBoughtFinalReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreBoughtFinalReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/peachy006/storeboughtfinalreact/StoreBoughtFinalReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.peachy006.storeboughtfinalreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBoughtFinalReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
